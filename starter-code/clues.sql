908566aebf6076607984cc70a3a350f093614b66
cbrads61 <cbrads61@gmail.com>
2016-10-24 15:32:41 -0400

    Carmen SQLite Lab

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,17 +2,28 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+——-SELECT * FROM country WHERE region = ’Southern Europe’ ORDER BY population;
+	VAT|Holy See (Vatican City State)|Europe|Southern Europe|0.40000001|1929|1000||9|\N|Santa Sede/Citt� del Vaticano|Independent Church State|Johannes Paavali II|3538|VA
+
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+——-SELECT “language” FROM countrylanguage WHERE countryside = ‘VAT’;
+	Italian
+
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+
+———SELECT countrycode FROM countrylanguage WHERE “language” = ‘Italian’ AND percentage = 100;
+	SMR
+———SELECT name FROM country WHERE code = ‘SMR’
+	San Marino
 
 
 
@@ -21,11 +32,17 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+———SELECT name FROM city WHERE countrycode = ‘SMR’;
+	Serravalle
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+
+———SELECT * FROM city WHERE name LIKE ’Serr%’;
+	265|Serra|BRA|Esp�rito Santo|302666
 
 
 
@@ -33,11 +50,20 @@
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+——-SELECT capital FROM country WHERE code = ‘BRA’;
+	211
+———SELECT name FROM city WHERE id = 211;
+	Bras�lia
+
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
+
+
+
+
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
@@ -52,6 +78,10 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+———SELECT * FROM city WHERE population = 91084;
+	4060|Santa Monica|USA|California|91084
 
 
--- She's in ____________________________!
+
+
+-- She's in _______SANTA MONICA, CA_______!
